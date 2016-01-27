1a6e4ae9aec5be2e97b1e8dd3adf6dfa39a96d23
houtbrion <houtbrion@gmail.com>
2016-01-28 08:28:50 +0900

    LEDモジュール用の技術情報のWikiのサンプルプログラムを追加

---
+++ LED/LED.py
@@ -0,0 +1,25 @@
+# GrovePi LED Blink example
+
+import time
+from grovepi import *
+
+# Connect the Grove LED to digital port D4
+led = 4
+
+pinMode(led,"OUTPUT")
+time.sleep(1)
+
+while True:
+    try:
+        #Blink the LED
+        digitalWrite(led,1)		# Send HIGH to switch on LED
+        time.sleep(1)
+
+        digitalWrite(led,0)		# Send LOW to switch off LED
+        time.sleep(1)
+
+    except KeyboardInterrupt:	# Turn LED off before stopping
+        digitalWrite(led,0)
+        break
+    except IOError:				# Print "Error" if communication error encountered
+        print "Error"
