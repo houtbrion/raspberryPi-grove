6955867d4c6458273452fcf3dea74a2436c94a6d
houtbrion <houtbrion@gmail.com>
2016-01-28 09:35:35 +0900

    光センサのサンプルプログラムを追加

---
+++ Light_Sensor/Light_Sensor.py
@@ -0,0 +1,40 @@
+# GrovePi+ & Grove Light Sensor & LED
+
+import time
+import grovepi
+
+# Connect the Grove Light Sensor to analog port A0
+# SIG,NC,VCC,GND
+light_sensor = 0
+
+# Connect the LED to digital port D4
+# SIG,NC,VCC,GND
+led = 4
+
+# Turn on LED once sensor exceeds threshold resistance
+threshold = 10
+
+grovepi.pinMode(light_sensor,"INPUT")
+grovepi.pinMode(led,"OUTPUT")
+
+while True:
+    try:
+        # Get sensor value
+        sensor_value = grovepi.analogRead(light_sensor)
+
+        # Calculate resistance of sensor in K
+        resistance = (float)(1023 - sensor_value) * 10 / sensor_value
+
+        if resistance > threshold:
+            # Send HIGH to switch on LED
+            grovepi.digitalWrite(led,1)
+        else:
+            # Send LOW to switch off LED
+            grovepi.digitalWrite(led,0)
+
+        print "sensor_value =", sensor_value, " resistance =", resistance
+        time.sleep(.5)
+
+    except IOError:
+        print "Error"
+
